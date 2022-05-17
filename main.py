0962cd097ab5d9b8a1177c4f78cf34c9c65fcfe6
Diego Morales <diego@m200la.com>
2022-05-17 11:17:31 -0300

    Agrego el main.py al repoitorio por primera vez

---
+++ main.py
@@ -0,0 +1,13 @@
+from paquete_matematica import algebra
+from paquete_matematica import ecuaciones as ecu
+
+
+print("hola")
+
+result_sum = algebra.sumar(1, 3)
+result_rest = algebra.restar (1, 2)
+result_ecu = ecu.sumar(1, 3)
+
+print(result_rest)
+print(result_sum)
+print(result_ecu)
